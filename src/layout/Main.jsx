c9949ab277a0d59c8f117cb8d5fbb2e1719c40e8
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-28 15:25:59 +0300

    Исправление ошибки в main

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -15,11 +15,11 @@ class Main extends React.Component {
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) =>
-        this.setState({ movies: data.Search, loading: false }).catch((err) => {
+        this.setState({ movies: data.Search, loading: false })).catch((err) => {
           console.error(err);
           this.setState({ loading: false });
-        })
-      );
+        
+  });
   }
 
   searchMovies = (str, type = 'all') => {
@@ -31,11 +31,11 @@ class Main extends React.Component {
     )
       .then((response) => response.json())
       .then((data) =>
-        this.setState({ movies: data.Search, loading: false }).catch((err) => {
+        this.setState({ movies: data.Search, loading: false })).catch((err) => {
           console.error(err);
           this.setState({ loading: false });
-        })
-      );
+        
+  });
   };
 
   render() {
